492237b3572df205545365c1a24ef277894ae8f6
SiBem0l <julianllinares1@gmail.com>
2025-08-30 10:18:29 -0300

    Vector TDA modificaciones en clase

--- test/main.c
+++ test/main.c
@@ -4,8 +4,13 @@
 
 int main()
 {
-    printf("Hello World");
     Vector* v = vector_create();
+    vector_push_back(v,1);
+    vector_push_back(v,2);
+    vector_push_back(v,3);
+    int lastItem = vector_pop_back(v);
+    vector_set(v,1,18);
+    int number = vector_get(v,1);
     vector_destroy(v);
     return 0;
 }
--- test/vector.c
+++ test/vector.c
@@ -13,11 +13,17 @@ struct Vector{
     int maxSize;
 };
 
+static int resize(Vector* v);
+
 Vector* vector_create(){
     Vector* newVector = (Vector*)malloc(sizeof(Vector));
-    newVector->elements = (VECTOR_ELEMENT*)calloc(DEFAULT_MAX_SIZE, sizeof(VECTOR_ELEMENT));
-    newVector->size = 0;
-    newVector->maxSize = 10;
+    if(newVector != NULL){    
+        newVector->elements = (VECTOR_ELEMENT*)calloc(DEFAULT_MAX_SIZE, sizeof(VECTOR_ELEMENT));
+        if(newVector->elements != NULL){
+            newVector->size = 0;
+            newVector->maxSize = 10;
+        }
+    }
     return newVector;
 }
 
@@ -27,28 +33,46 @@ void vector_destroy(Vector* v){
 }
 
 int vector_get(Vector* v, int index){
-    VECTOR_ELEMENT value = v->elements[index];
-    (v->elements)[index] = VECTOR_ELEMENT_DEFAULT;
+    VECTOR_ELEMENT value = VECTOR_ELEMENT_DEFAULT;
+    if(index < v->size && index >= 0){
+        value = v->elements[index];
+    }
     return value;
 }
 
 void vector_set(Vector* v, int index, int value){
-    v->elements[index] = value;
+    if(index < v->size && index >= 0){
+        v->elements[index] = value;
+    }
 }
 
-void vector_push_back(Vector* v, int value){
-    if(v->size >= v->maxSize){ //aumentar capacidad del vector al doble de su capacidad
-        v->elements = (VECTOR_ELEMENT*)realloc(v->elements, sizeof(VECTOR_ELEMENT)*v->size*2);
+static int resize(Vector* v){
+    int done = 0;
+    v->elements = (VECTOR_ELEMENT*)realloc(v->elements, sizeof(VECTOR_ELEMENT)*v->size*2);
+    if(v->elements != NULL){
+        done = 1;
     }
-    v->elements[v->size] = value;
-    v->size++;
+    return done;
+}
+void vector_push_back(Vector* v, int value){
+    if(v != NULL){
+        if(v->size >= v->maxSize){ //aumentar capacidad del vector al doble de su capacidad
+            if(resize(v) == 0){
+                return; 
+            }
+        }
+        v->elements[v->size] = value;
+        v->size++;
+    }
 }
 int vector_pop_back(Vector* v){
     VECTOR_ELEMENT value = VECTOR_ELEMENT_DEFAULT;
-    if(v->size != 0){
-        v->size--;
-        value = v->elements[v->size];
-        v->elements[v->size] = VECTOR_ELEMENT_DEFAULT;
+    if(v != NULL){
+        if(v->size != 0){
+            v->size--;
+            value = v->elements[v->size];
+            v->elements[v->size] = VECTOR_ELEMENT_DEFAULT;
+        }
     }
     return value;
 }
