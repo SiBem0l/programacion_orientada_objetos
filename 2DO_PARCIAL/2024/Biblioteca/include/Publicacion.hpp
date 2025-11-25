390b3596d2c186fc76c5a7014a36928ec1d830dc
SiBem0l <julianllinares1@gmail.com>
2025-11-25 16:09:11 -0300

    Examen 2024: correciones Pablo

--- 2DO_PARCIAL/2024/Biblioteca/include/Publicacion.hpp
+++ 2DO_PARCIAL/2024/Biblioteca/include/Publicacion.hpp
@@ -8,35 +8,25 @@
 
 class Publicacion
 {
-private:
+protected:
     std::string titulo;
     std::string autor;
     Fecha fechaPublicacion;
 
-protected:
-    Publicacion(std::string titulo, std::string autor, Fecha fechaPublicacion);
+    void guardarBinario(std::ofstream &outStream) const;
 
 public:
     using dia_type = Fecha::dia_type;
     using mes_type = Fecha::mes_type;
     using anio_type = Fecha::anio_type;
 
-    const std::string &getTitulo() const;
-    const std::string &getAutor() const;
-    Fecha getFechaPublicacion() const;
-    dia_type getDiaPublicacion() const;
-    mes_type getMesPublicacion() const;
-    anio_type getAnioPublicacion() const;
+    template <typename T, typename U>
+    Publicacion(T&& titulo, U&& autor, Fecha fecha);
+    virtual ~Publicacion() = default;
 
-    void setTitulo(std::string titulo);
-    void setAutor(std::string autor);
-    void setFechaPublicacion(Fecha fecha);
-    void setDiaPublicacion(dia_type dia);
-    void setMesPublicacion(mes_type mes);
-    void setAnioPublicacion(anio_type anio);
 
-    virtual void guardarBinario(std::ofstream &outStream) const;
-    static std::unique_ptr<Publicacion> leerBinario(std::ifstream &inStream);
+    virtual void guardar(std::ofstream &outStream) const = 0;
+    static std::unique_ptr<Publicacion> leer(std::ifstream &inStream);
 
     static void guardarString(const std::string &s, std::ofstream &outStream);
 };
--- 2DO_PARCIAL/2024/Biblioteca/src/Publicacion.cpp
+++ 2DO_PARCIAL/2024/Biblioteca/src/Publicacion.cpp
@@ -2,76 +2,19 @@
 #include <iostream>
 #include <stdexcept>
 
-Publicacion::Publicacion(std::string titulo, std::string autor, Fecha fecha)
-    : titulo(std::move(titulo)), autor(std::move(autor)), fechaPublicacion(fecha)
+template <typename T, typename U>
+Publicacion::Publicacion(T &&titulo, U &&autor, Fecha fecha)
+    : titulo(std::forward<T>(titulo)),
+      autor(std::forward<U>(autor)),
+      fechaPublicacion(fecha)
 {
 }
 
-// Getters
-const std::string &Publicacion::getTitulo() const
-{
-    return titulo;
-}
 
-const std::string &Publicacion::getAutor() const
-{
-    return autor;
-}
-
-Fecha Publicacion::getFechaPublicacion() const
-{
-    return fechaPublicacion;
-}
-
-Publicacion::dia_type Publicacion::getDiaPublicacion() const
-{
-    return fechaPublicacion.getDia();
-}
-
-Publicacion::mes_type Publicacion::getMesPublicacion() const
-{
-    return fechaPublicacion.getMes();
-}
-
-Publicacion::anio_type Publicacion::getAnioPublicacion() const
-{
-    return fechaPublicacion.getAnio();
-}
-
-void Publicacion::setTitulo(std::string t)
-{
-    titulo = std::move(t);
-}
-
-void Publicacion::setAutor(std::string a)
-{
-    autor = std::move(a);
-}
-
-void Publicacion::setFechaPublicacion(Fecha f)
-{
-    fechaPublicacion = std::move(f);
-}
-
-void Publicacion::setDiaPublicacion(dia_type dia)
-{
-    fechaPublicacion.setDia(dia);
-}
-
-void Publicacion::setMesPublicacion(mes_type mes)
-{
-    fechaPublicacion.setMes(mes);
-}
-
-void Publicacion::setAnioPublicacion(anio_type anio)
-{
-    fechaPublicacion.setAnio(anio);
-}
-
-static void guardarString(const std::string& s, std::ofstream &outStream)
+static void guardarString(const std::string &s, std::ofstream &outStream)
 {
     std::string::size_type lenght = s.size();
-    outStream.write(reinterpret_cast<const char*>(&lenght), sizeof(lenght));
+    outStream.write(reinterpret_cast<const char *>(&lenght), sizeof(lenght));
     outStream.write(s.data(), lenght);
 }
 void Publicacion::guardarBinario(std::ofstream &outStream) const
@@ -90,13 +33,13 @@ static std::string leerString(std::ifstream &inStream)
 {
     std::string s;
     std::string::size_type lenght;
-    inStream.read(reinterpret_cast<char*>(&lenght), sizeof(lenght));
+    inStream.read(reinterpret_cast<char *>(&lenght), sizeof(lenght));
     s.resize(lenght);
     inStream.read(s.data(), lenght);
 
-    return std::move(s);
+    return s;
 }
-std::unique_ptr<Publicacion> Publicacion::leerBinario(std::ifstream &inStream)
+std::unique_ptr<Publicacion> Publicacion::leer(std::ifstream &inStream)
 {
     if (inStream.fail())
     {
@@ -107,7 +50,9 @@ std::unique_ptr<Publicacion> Publicacion::leerBinario(std::ifstream &inStream)
     std::string autor(leerString(inStream));
     Fecha fechaPublicacion(Fecha::leerBinario(inStream));
 
-    return std::make_unique<Publicacion>(std::move(titulo),
-                                         std::move(autor),
-                                         fechaPublicacion);
+    std::unique_ptr ptr = std::make_unique<Publicacion>(std::move(titulo),
+                                                        std::move(autor),
+                                                        fechaPublicacion);
+
+    return ptr;
 }
